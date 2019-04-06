01755e1a7c0703d4fd72fa331282e810e515d90b
AsmaaZakaria <asmaazakaria94@gmail.com>
2019-04-06 18:45:09 +0200

    add animation to services

--- src/containers/Services.js
+++ src/containers/Services.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles, Grid, Typography } from '@material-ui/core';
 import { Spring } from 'react-spring/renderprops';
+import VisibilitySensor from "react-visibility-sensor";
 
 import content1 from '../images/content/content-001.png';
 import content2 from '../images/content/content-002.png';
@@ -36,49 +37,60 @@ function Services(props) {
         </Grid>
         <Grid item sm>
           <div className={classes.contentContainer}>
-              <Spring
-                from={{ opacity: 0 }}
-                to={{ opacity: 1 }}
-                config={{ duration: 2000 }}
-              >
-              {props => <div style={props}>
-                <Typography component="h3" variant="h4" gutterBottom color="primary">
-                  We can give you our best user experience to your system
-                </Typography>
-              </div>}
-            </Spring>
-            <Typography variant="h6" color="textSecondary" component="p">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-              It has survived not only five centuries, but also the leap into electronic typesetting,
-              remaining essentially unchanged
+            <Typography component="h3" variant="h4" gutterBottom color="primary">
+              We can give you our best user experience to your system
             </Typography>
+            <VisibilitySensor offset={{ top: -500 }}>
+             {({ isVisible }) => (
+               <Spring
+                 delay={100}
+                 to={{
+                   opacity: isVisible ? 1 : 0,
+                   transform: isVisible ? "translateX(0)" : "translateX(100px)"
+                 }}
+               >
+                 {props =>
+                   <Typography variant="h6" color="textSecondary" component="p" style={{ ...props }}>
+                     Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                     when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                     It has survived not only five centuries, but also the leap into electronic typesetting,
+                     remaining essentially unchanged
+                   </Typography>}
+               </Spring>
+             )}
+           </VisibilitySensor>
+
           </div>
         </Grid>
       </Grid>
       <Grid container className={classes.gridContainer} style={{paddingTop: '30px'}} sm={12} justify="center" alignItems="center" spacing={0}>
         <Grid item sm>
           <div className={classes.contentContainer}>
-            <Spring
-              from={{ opacity: 0 }}
-              to={{ opacity: 1 }}
-              config={{ duration: 2000 }}
-            >
-              {props => <div style={props}>
-                <Typography component="h3" variant="h4" gutterBottom color="primary">
-                  Easy access.
-                  Whenever, wherever you want
-                </Typography>
-              </div>}
-            </Spring>
-            <Typography variant="h6" color="textSecondary" component="p">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-              It has survived not only five centuries, but also the leap into electronic typesetting,
-              remaining essentially unchanged
+            <Typography component="h3" variant="h4" gutterBottom color="primary">
+              Easy access.< br/>
+              Whenever, wherever you want
             </Typography>
+            <VisibilitySensor offset={{ top: -500 }}>
+             {({ isVisible }) => (
+               <Spring
+                 delay={50}
+                 to={{
+                   opacity: isVisible ? 1 : 0,
+                   transform: isVisible ? "translateX(0)" : "translateX(400px)"
+                 }}
+               >
+                 {props =>
+                   <Typography variant="h6" color="textSecondary" component="p" style={{ ...props }}>
+                     Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                     when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                     It has survived not only five centuries, but also the leap into electronic typesetting,
+                     remaining essentially unchanged
+                   </Typography>}
+               </Spring>
+             )}
+           </VisibilitySensor>
           </div>
         </Grid>
         <Grid item>
